7eac829df6eba3bb56292222e4c74f1712b69053
bebetodo69 <rychard.phelipe@escolar.ifrn.edu.br>
2025-11-24 13:36:41 -0300

    47% feito

--- Project MEMIZ/Assets/Script/Mosntro.cs
+++ Project MEMIZ/Assets/Script/Mosntro.cs
@@ -1,16 +1,29 @@
 using UnityEngine;
 
-public class Mosntro : MonoBehaviour
+public class MonsterWalk : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public float speed = 2f; 
+    public float distance = 5f; 
+    private Vector3 startPosition;
+    private bool movingRight = true;
+
     void Start()
     {
-        
+        startPosition = transform.position;
     }
 
-    // Update is called once per frame
     void Update()
     {
+        Vector3 direction = movingRight ? Vector3.right : Vector3.left;
+        transform.Translate(direction * speed * Time.deltaTime);
         
+        if (movingRight && transform.position.x >= startPosition.x + distance)
+        {
+            movingRight = false;
+        }
+        else if (!movingRight && transform.position.x <= startPosition.x - distance)
+        {
+            movingRight = true;
+        }
     }
-}
+}  
